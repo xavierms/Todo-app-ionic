f23c14adc788615ed3cc3110fe72694f42cc2132
xavierms <xavierams08@gmail.com>
2022-11-07 14:17:57 -0400

    todo: creating reactive form...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+export interface Todo{
+  itemName: string;
+  itemDueDate: string;
+  itemPriority: string;
+  itemCategory: string;
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +14,61 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    // this.todos = [
+    //   {
+    //     itemName:'coding',
+    //     itemDueDate:'07-11-2022',
+    //     itemPriority:'high',
+    //     itemCategory:'Work'
+    //   },
+    //   {
+    //     itemName:'other',
+    //     itemDueDate:'07-11-2022',
+    //     itemPriority:'low',
+    //     itemCategory:'Work'
+    //   },
+    //   {
+    //     itemName:'other x2',
+    //     itemDueDate:'07-11-2022',
+    //     itemPriority:'middle',
+    //     itemCategory:'Work'
+    //   },
+    //   {
+    //     itemName:'other x2',
+    //     itemDueDate:'07-11-2022',
+    //     itemPriority:'middle',
+    //     itemCategory:'Work'
+    //   },
+    //   {
+    //     itemName:'other x2',
+    //     itemDueDate:'07-11-2022',
+    //     itemPriority:'middle',
+    //     itemCategory:'Work'
+    //   },
+    // ]
+  }
+  form:FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+  })
+  todos: Todo[] = [];
 
+  today: number = Date.now();
+  submitForm() {
+
+  }
+  onWillDismiss(e: any){
+    console.log(e);
+    
+  }
+  cancel(){
+
+  }
+
+  confirm(){
+
+  }
+  isColorPriority(priority: string){
+  return priority === 'high' ? 'red':(priority === 'low'?'green':'orange')
+  }
 }
