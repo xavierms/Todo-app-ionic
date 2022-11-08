7d288dffae5b2cf5abe5e387c4b8566daf3a0c8d
xavierms <xavierams08@gmail.com>
2022-11-08 18:17:14 -0400

    todo cad component added

---
+++ src/app/home/todo-card/todo-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo';
+
+@Component({
+  selector: 'app-todo-card',
+  templateUrl: './todo-card.component.html',
+  styleUrls: ['./todo-card.component.scss'],
+})
+export class TodoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+  
+  @Input() todos: Todo[] = [];
+
+  isColorPriority(priority: string){
+    return priority === 'high' ? 'red':(priority === 'low'?'green':'orange');
+  }
+}
