3659a7b3dd9b391d8b19c0ea4a4c15a413e0714d
xavierms <xavierams08@gmail.com>
2022-11-08 18:16:42 -0400

    header added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { HeaderComponent } from "./header/header.component";
+
+
+@NgModule({
+    declarations:[HeaderComponent],
+    exports:[HeaderComponent],
+    imports:[]
+})
+export class SharedModule{
+
+}
